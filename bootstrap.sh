7c35169c53e3c515330be68ad6445b14b6a74e8d
Marco Rolando <marco.rolando@gmail.com>
2015-05-09 09:32:43 +0100

    Added support for rsync dry run

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,15 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+	rsync  -zvh backup.tar /tmp/backups/ \
+		--exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude ".osx" \
+		--exclude "brew.sh" -avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+function testIt() {
+	rsync --dry-run -zvh backup.tar /tmp/backups/ \
+		--exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude ".osx" \
 		--exclude "brew.sh" -avh --no-perms . ~;
 	source ~/.bash_profile;
@@ -13,6 +21,8 @@ function doIt() {
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
+elif [ "$1" == "--test" -o "$1" == "-t" ]; then
+	testIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
